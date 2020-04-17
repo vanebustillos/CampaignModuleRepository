12ad970929581b42fb18402e7e003d403478f5a8
vane_bustillos <vanebustillos1@gmail.com>
2020-04-17 17:14:31 -0400

    Adding DTO Models.

---
+++ CampaignModule/Controllers/DTOModels/CampaignDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampaignModule.Controllers.DTOModels
+{
+    public class CampaignDTO
+    {
+        public string CampaignName { get; set; }
+        public string CampaignType { get; set; }
+        public string CampaignDescription { get; set; }
+    }
+}
