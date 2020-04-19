335f6b23601fc553841afec79dd713d97c802cdf
RUBÉN MALDONADO <romancalli@hotmail.com>
2020-04-19 18:13:45 -0400

    Implementate the class CampaignLogic and fix the problems in the Interface Logic

--- CampaignModule/BusinessLogic/ICampaignLogic.cs
+++ CampaignModule/BusinessLogic/ICampaignLogic.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CampaignModule.Controllers.DTOModels;
 
-namespace CampaignModule.Business_Logic
+namespace CampaignModule.BusinessLogic
 {
     public interface ICampaignLogic
     {
