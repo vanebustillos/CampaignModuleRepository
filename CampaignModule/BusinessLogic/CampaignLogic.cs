5c52253f8f26ae8fe091cac2eb52b3bbfaf592ae
RUBÉN MALDONADO <romancalli@hotmail.com>
2020-04-19 17:40:31 -0400

    Create Interface and Class of BusinessLogic

---
+++ CampaignModule/BusinessLogic/CampaignLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampaignModule.BusinessLogic
+{
+    public class CampaignLogic
+    {
+    }
+}
