87e89bf7b6a1ae8486be67671fbe38d4dd69ddb6
Brami01 <bramopit@gmail.com>
2020-04-21 14:36:58 -0400

    files moved

--- BusinessLogic/BusinessLogic/ICampaignLogic.cs
+++ BusinessLogic/BusinessLogic/ICampaignLogic.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CampaignModule.Controllers.DTOModels;
 using CampaignModule.Database.Models;
 
 namespace CampaignModule.BusinessLogic
--- CampaignModule/Controllers/CampaignController.cs
+++ CampaignModule/Controllers/CampaignController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using CampaignModule.BusinessLogic; //To be added
 using CampaignModule.Database.Models;
+using CampaignModule.BusinessLogic;
 
 namespace CampaignModule.Controllers
 {
