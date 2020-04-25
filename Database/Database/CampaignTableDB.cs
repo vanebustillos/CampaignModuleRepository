f803c0009aacfca8de3a09c0f9fa9c5172e94e4e
Brami01 <bramopit@gmail.com>
2020-04-24 22:13:17 -0400

    Implemented the configuration files loading

--- Database/Database/CampaignTableDB.cs
+++ Database/Database/CampaignTableDB.cs
@@ -10,18 +10,19 @@ namespace CampaignModule.Database
         {
                 new Campaign() { Id = 1,Name = "Christmas Part I", Type = "XMAS", Description = "Before 24th December", Active = false},
                 new Campaign() { Id = 2,Name = "Christmas Part II", Type = "XMAS", Description = "After 25th December", Active = false},
-                new Campaign() { Id = 3,Name = "Easter", Type = "SPRING", Description = "Resurrection of Jesus Christ between March 21 and April 25", Active = true},
-                new Campaign() { Id = 4,Name = "Summer discounts", Type = "SUMMER", Description = "All July", Active = false},
-                new Campaign() { Id = 5,Name = "Black Friday discounts", Type = "BFRIDAY", Description = "Buy all you can", Active = false}
+                new Campaign() { Id = 3,Name = "Summer discounts", Type = "SUMMER", Description = "All July", Active = false},
+                new Campaign() { Id = 4,Name = "Black Friday discounts", Type = "BFRIDAY", Description = "Buy all you can", Active = false}
         };
+
         public List<Campaign> GetAll() //Read, returns all campaigns
         {
             return DataBase;
         }
 
-        public void Create(Campaign campaign) // Creates a New Campaign 
+        public Campaign Create(Campaign campaign) // Creates a New Campaign 
         {
             DataBase.Add(campaign);
+            return campaign;
         }
 
         public void Update(Campaign campaign) //Updates all fields in a Campaign except its id
@@ -30,7 +31,6 @@ namespace CampaignModule.Database
             {
                 if (camp == campaign)
                 {
-                    
                     camp.Name = campaign.Name;
                     camp.Type = campaign.Type;
                     camp.Description = campaign.Description;
--- Database/Database/ICampaignTableDB.cs
+++ Database/Database/ICampaignTableDB.cs
@@ -7,7 +7,7 @@ namespace CampaignModule.Database
     public interface ICampaignTableDB
     {
         public List<Campaign> GetAll(); //Get, Read all in Database
-        public void Create(Campaign campaign); //Post
+        public Campaign Create(Campaign campaign); //Post
         public void Update(Campaign campaign); //Put
         public void Delete(Campaign campaign); //Delete
         public bool OneCampaignActive();//Verify if any campaign is actives
