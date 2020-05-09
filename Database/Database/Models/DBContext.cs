0b751b86877167c2db532dab02e0be4b0e1e0822
Rodrigo Heredia <rodrigoh1205@gmail.com>
2020-05-09 12:28:09 -0400

    Added Task 12 - DB as an external service

---
+++ Database/Database/Models/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using CampaignModule.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Database.Models
+{
+    public class DBContext
+    {
+        public List<Campaign> campaigns { get; set; }
+    }
+}
